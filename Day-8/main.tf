54dd4094667519f01e3ba11847afdd6b5c7965ba
sanju biswas <105317380+sanjubiswad@users.noreply.github.com>
2024-03-06 14:06:21 +0530

    Update main.tf

--- Day-8/main.tf
+++ Day-8/main.tf
@@ -1,1 +1,16 @@
+provider "aws" {
+    region = "us-east-1"
+}
 
+import {
+  id = "i-0947784663686"
+  to = aws_instance.exmaple
+}
+
+
+
+
+command
+-------------------
+terraform init
+terraform plan -generate-config-out=generated_resource.tf
